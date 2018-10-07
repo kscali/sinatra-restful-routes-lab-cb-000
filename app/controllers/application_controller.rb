8ddb09ece3ac528d158c1cc16878216d921106a3
Koy Saeteurn <kosatun@aol.com>
2018-10-07 02:30:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
     @recipes.ingredients = params[:ingredients]
     @recipes.cook_time = params[:cook_time]
     @recipes.save
-    redirect '/show'
+    redirect "/recipes/#{@recipes.id}"
     end  
     
     delete '/recipes/:id/delete' do
